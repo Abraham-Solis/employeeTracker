930ceee15a82900c055c643366bb8f7bbaed415c
email <abe.solis2015@gmail.com>
2021-12-15 20:41:08 -0800

    Added Scheema to VSCODE

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+
+CREATE TABLE employees(
+	ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL
+);
+
+
+USE employeeTracker_db;
+
+
+CREATE TABLE roles (
+ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+rolesName VARCHAR(50) NOT NULL,
+salary DECIMAL (10,2) NOT NULL,
+department_id INT NOT NULL
+
+);
+
+
+USE employeeTracker_db;
+
+
+CREATE TABLE departments (
+ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(50) NOT NULL
+);
