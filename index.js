03af8fab6049f68b4241393fe3daaca86b286d51
email <abe.solis2015@gmail.com>
2021-12-18 00:57:16 -0800

    add manager prompt

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const mainPrompt = () => {
       type: 'list',
       name: 'choice',
       message: 'What would you like to do?',
-      choices: ['View Departments', 'View Roles', 'View Employees', 'Add a Department', 'Add a Employee', 'Add a Role', 'Update Employee Roles']
+      choices: ['View Departments', 'View Roles', 'View Employees', 'Add a Department', 'Add an Employee', 'Add a Role', 'Update Employee Roles']
     },
   ])
 
@@ -36,6 +36,10 @@ const mainPrompt = () => {
 
         case 'Add a Department':
           addDepartment()
+          break;
+
+        case 'Add an Employee':
+          addEmployees()
           break;
       }
 
@@ -79,11 +83,45 @@ function seeDepartments() {
 
 //ADD Functions for Employees 
 
-function addEmployees(newEmployee) {
+function addEmployees() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: 'Please enter employees First Name'
+
+
+    },
+
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'Please enter employees Last Name'
+
+    },
+
+    {
+      type: 'input',
+      name: 'role_id',
+      message: 'Please enter employees Role ID'
+
+    },
+
+    {
+      type: 'input',
+      name: 'manager_id',
+      message: 'Please enter employees Manager ID'
+
+    }
+  ])
+
+  .then(newEmployee => {
+    console.log(newEmployee)
   db.query('INSERT INTO employees SET ?', newEmployee, err => {
     if (err) { console.log(err) }
     console.log('You made an Employee!')
-  })
+    mainPrompt()
+  })})
 }
 
 
@@ -99,11 +137,23 @@ function addRole(newRole) {
 //ADD Functions for Department
 
 function addDepartment() {
-  inquirer.prompt 
-  db.query('INSERT INTO departments SET ?', newDepartment, err => {
-    if (err) { console.log(err) }
-    console.log('You added a Department!')
-  })
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Please name a New Department.'
+    }
+
+  ])
+
+    .then(newDepartment => {
+      console.log(newDepartment)
+      db.query('INSERT INTO departments SET ?', newDepartment, err => {
+        if (err) { console.log(err) }
+        console.log('You added a Department!')
+        mainPrompt()
+      })
+    })
 }
 
 
